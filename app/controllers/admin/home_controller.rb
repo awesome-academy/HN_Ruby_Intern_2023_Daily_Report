504623dc840d6319a22943c075996548f426634e
VuXuanBac <vuxuanbac712@gmail.com>
2023-12-24 00:50:43 +0700

    Export Library Data

--- app/controllers/admin/home_controller.rb
+++ app/controllers/admin/home_controller.rb
@@ -2,4 +2,16 @@ class Admin::HomeController < Admin::BaseController
   def index
     @period = params[:period]&.to_sym || :week
   end
+
+  def export_library
+    respond_to do |format|
+      format.xlsx do
+        @books = Book.includes_info
+        @publishers = Publisher.all
+        @authors = Author.with_attached_avatar
+        @genres = Genre.all
+        render xlsx: "books", template: "admin/home/export_library"
+      end
+    end
+  end
 end
