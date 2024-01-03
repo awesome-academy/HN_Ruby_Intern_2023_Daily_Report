1b090239d7d994a27f8208c0cd19a82471ddec9d
nmtri2810 <nmtri281010@gmail.com>
2024-01-03 17:12:35 +0700

    [Rspec] authors_controller

--- spec/requests/authors_spec.rb
+++ spec/requests/authors_spec.rb
@@ -1,11 +1,26 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe "Authors", type: :request do
+RSpec.describe AuthorsController, type: :controller do
   describe "GET /show" do
-    it "returns http success" do
-      get "/authors/show"
-      expect(response).to have_http_status(:success)
-    end
-  end
+    context "when author id valid" do
+      it "render show" do
+        author = create :author
 
+        get :show, params: { id: author.id}
+        expect(response).to render_template(:show)
+      end
+    end
+
+    context "when author not found" do
+      before { get :show, params: {id: -1} }
+
+      it "display flash warning " do
+        expect(flash[:warning]).to eq I18n.t("author_not_found")
+      end
+
+      it "redirect root page" do
+        expect(response).to redirect_to root_path
+      end
+      end
+    end
 end
