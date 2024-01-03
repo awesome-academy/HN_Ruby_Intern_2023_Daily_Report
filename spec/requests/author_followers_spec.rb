71ec14ecb1f2a007a7dab1030e6094fe42ad73db
nmtri2810 <nmtri281010@gmail.com>
2024-01-04 01:36:41 +0700

    [Rspec] author_followers_controller

--- spec/requests/author_followers_spec.rb
+++ spec/requests/author_followers_spec.rb
@@ -1,18 +1,57 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe "AuthorFollowers", type: :request do
-  describe "GET /create" do
-    it "returns http success" do
-      get "/author_followers/create"
-      expect(response).to have_http_status(:success)
+RSpec.describe AuthorFollowersController, type: :controller do
+  let(:account) { create(:account) }
+  let(:author) { create(:author) }
+
+  before(:each) do
+    allow(controller).to receive(:authenticate_account!).and_return(true)
+    allow(controller).to receive(:current_account).and_return(account)
+  end
+
+  shared_examples "displays flash message" do |flash_type, message|
+    it "displays flash #{flash_type}" do
+      expect(flash.now[flash_type]).to eq I18n.t(message)
     end
   end
 
-  describe "GET /delete" do
-    it "returns http success" do
-      get "/author_followers/delete"
-      expect(response).to have_http_status(:success)
+  describe "POST #create" do
+    context "when author id valid" do
+      before { post :create, params: {id: author.id} }
+
+      it "follows the author" do
+        expect(account.following?(author)).to be_truthy
+      end
+
+      it_behaves_like "displays flash message", :success, "author_follow_successfully"
+
+      it "redirects to the author page" do
+        expect(response).to redirect_to author_path(author)
+      end
+    end
+
+    context "when author not found" do
+      before { post :create, params: {id: -1} }
+
+      it_behaves_like "displays flash message", :warning, "author_not_found"
+
+      it "redirect to root page" do
+        expect(response).to redirect_to root_path
+      end
     end
   end
 
+  describe "DELETE #destroy" do
+    before { delete :destroy, params: {id: author.id} }
+
+    it "unfollows the author" do
+      expect(account.following?(author)).to be_falsey
+    end
+
+    it_behaves_like "displays flash message", :success, "author_unfollow_successfully"
+
+    it "redirects to the author page" do
+      expect(response).to redirect_to author_path(author)
+    end
+  end
 end
